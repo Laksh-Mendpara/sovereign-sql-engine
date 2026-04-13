9d8533252fc5523d7ee84698a8d9094755b12b73
laksh <b23cs1037@iitj.ac.in>
2026-04-13 13:03:28 +0530

    Fixed: quantization config

--- vllm_worker/build.sh
+++ vllm_worker/build.sh
@@ -4,7 +4,6 @@
 docker build -t worker-vllm-baked \
   --build-arg MODEL_NAME="ByteMaster01/arcitic-8q-tensorizer" \
   --build-arg BASE_PATH="/models" \
-  --build-arg QUANTIZATION="gptq" \
   --build-arg MAX_MODEL_LEN="4096" \
   --build-arg ENFORCE_EAGER="true" \
   .
